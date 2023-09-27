ee4964623ea5ecace500e68f9d55b83d8429ddf7
nguyenvancongdev <congthang.nuce@gmail.com>
2023-09-27 19:42:47 +0700

    update

---
+++ lib/widgets/user_card.dart
@@ -0,0 +1,23 @@
+// import 'package:flutter/material.dart';
+// import '../models/user.dart';
+
+// class UserCard extends StatelessWidget {
+//   final User user;
+
+//   UserCard({Key key, this.user}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       child: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Column(
+//           children: [
+//             Text(user.name),
+//             Text(user.email),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
