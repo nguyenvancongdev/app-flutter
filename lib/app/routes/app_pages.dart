38b6ad424e5deb2d36a90dcde1ae7739a631fd3e
nguyenvancongdev <congthang.nuce@gmail.com>
2023-10-04 22:33:45 +0700

    rr

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,18 +1,18 @@
 import 'package:get/get.dart';
-import '../pages/home/widget;
-part 'app_routes.dart';
+import '../pages/home/views/home_view.dart';
+part './app_routes.dart';
 class AppPages {
   static const INTIAL = ROUTER.HOME;
   static final router = [
     GetPage(
-      name: _Paths.HOMES;
-      page: () => HomeView()
+      name: _Paths.HOMES,
+      page: () => HomeView(),
       binding: HomeBinding()
     ),
     GetPage(
-      name: _Paths.LOGIN;
-      page: () => HomeView()
-      binding: HomeBinding()
+      name: _Paths.LOGIN,
+      page: () => LoginView(),
+      binding: HomeBinding(),
     ),
 
   ]
